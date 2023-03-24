1b6d7b2b4d325a7c342b80799ced8efc3ad4409b
Turbold <turuu.boldoo@gmail.com>
2023-03-24 15:36:20 +0800

    dependency update

--- app/src/main/java/mn/turbo/data/repository/DefaultTodoRepository.kt
+++ app/src/main/java/mn/turbo/data/repository/DefaultTodoRepository.kt
@@ -2,6 +2,9 @@ package mn.turbo.data.repository
 
 import mn.turbo.data.remote.TodoApi
 
-class DefaultTodoRepository(private val api: TodoApi) : TodoRepository {
-    override suspend fun getTodoList() = api.fetchSomething()
+class DefaultTodoRepository(
+    private val api: TodoApi
+) : TodoRepository {
+    override suspend fun getTodoList() =
+        api.fetchSomething()
 }
